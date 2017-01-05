896519edbe7939098cb10e89e15bad1fb23195ac
Sandeep Rawat <sandeep@opstree.com>
2017-01-05 16:39:32 +0530

    Updated mysql recipe

--- scm/puppet/recipes/app/mysql.pp
+++ scm/puppet/recipes/app/mysql.pp
@@ -7,15 +7,19 @@ service {"mysql":
 }
 
 exec {"create database":
+     require => Service['mysql'],
     command => '/usr/bin/mysql -e "CREATE DATABASE IF NOT EXISTS employeedb"'
 }
 
 
 exec {"Set root user password":
+    require => Service['mysql'],
     command => '/usr/bin/mysql -uroot -e "SET PASSWORD FOR \'root\'@\'%\' = PASSWORD(\'password\');"'
 }
 
 exec {"Set root user password for local":
+    require => Service['mysql'],
     command => '/usr/bin/mysql -uroot -e "SET PASSWORD FOR \'root\'@\'localhost\' = PASSWORD(\'password\');"'
 }
+
 
