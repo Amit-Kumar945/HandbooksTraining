27dea292b2b04a16aa419e3d7b7bf6e98ce2d729
saurabh.vaj <saurabh.vaj>
2017-01-06 12:49:35 +0530

    added deployment script

--- scripts/deployer.sh
+++ scripts/deployer.sh
@@ -4,10 +4,10 @@ sleep 10
 
 echo "Delete old version of app"
 
-rm -r /opt/tomcat/webapps/ROOT/* || rm  -r /var/lib/tomcat7/webapps/ROOT/*
+rm -r /opt/tomcat/webapps/* || rm  -r /var/lib/tomcat7/webapps/*
 
 echo "Copy artifat from temporary location to tomcat root"
-cp /tmp/*.war /opt/tomcat/webapps/ROOT/  ||  cp /tmp/*.war /var/lib/tomcat7/webapps/ROOT/
+cp /tmp/*.war /opt/tomcat/webapps/  ||  cp /tmp/*.war /var/lib/tomcat7/webapps/
 
 echo "Start tomcat server again"
 /opt/tomcat/bin/startup.sh ||  service tomcat7 start
