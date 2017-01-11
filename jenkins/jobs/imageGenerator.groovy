391545755eefcd16caa0a276ffe0384d5b6398df
Sandeep Rawat <sandeep@opstree.com>
2017-01-11 17:11:40 +0530

    Changed the branch from master to dockerization so that we can generate docker image

---
+++ jenkins/jobs/imageGenerator.groovy
@@ -0,0 +1,17 @@
+mavenJob("ImageGenerator") {
+  logRotator(60, 20, 1, -1)
+  description('I will generate artifact of application')
+  scm {
+   		git {
+     		branch('*/dockerization')
+     		remote {
+       			name('')
+       			refspec('')
+       			url('https://github.com/OpsTree/ContinuousIntegration.git')
+     		}
+   		}
+ 	}
+  goals('clean package')
+  rootPOM('Spring3HibernateApp/pom.xml')
+  mavenInstallation('maven2')
+}
