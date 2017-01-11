e32ad9c1e5b1cf5d423def5281a8189470235a92
Sandeep Rawat <sandeep@opstree.com>
2017-01-11 13:49:11 +0530

    Updating poll SCM job to poll the changes in any job

--- jenkins/jobs/pollSCM.groovy
+++ jenkins/jobs/pollSCM.groovy
@@ -1,10 +1,20 @@
 job('Poll SCM') {
 	description("I\'ll do the poll scm in every 15 min")
     logRotator(-1, 10)
-	scm {
-        github('https://github.com/OpsTree/ContinuousIntegration.git', 'master')
-    }
+		scm {
+	   		git {
+	     		branch('*/master')
+	     		remote {
+	       			name('')
+	       			refspec('')
+	       			url('https://github.com/OpsTree/HandbooksTraining.git')
+	     		}
+	   		}
+	 	}
   	triggers {
         scm('H/15 * * * *')
     }
+		steps {
+        shell('#!/bin/bash \n cp jobs/*.groovy ${JENKINS_HOME}/jobs/SeedJob/workspace/jobs')
+		}
 }
